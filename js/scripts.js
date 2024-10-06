e79cc4f0d8b3e44d00474b3351ebe9f8c2d415f0
fixdet2024 <73460689@institutocajas.info>
2024-10-06 16:41:52 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { numero: 2, subtareas: ["Wireframes", "Prototipo"], githubUrl: "#" },
         { numero: 3, subtareas: ["Configuración del entorno", "Creación de la base de datos"], githubUrl: "https://github.com/fixdet2024/Aplicacion-Movil/tree/main/ejercicios%20semana3"},
         { numero: 4, subtareas: ["Con", "Creación de la base de datos"], githubUrl: "https://github.com/fixdet2024/Aplicacion-Movil/tree/main/ejercicios%20semana4" },
-        { numero: 5, subtareas: ["Investigación", "Planificación"], githubUrl: "https://github.com/tuUsuario/tuRepositorio/semana1" },
+        { numero: 5, subtareas: ["Investigación", "Planificación"], githubUrl: "https://github.com/fixdet2024/Aplicacion-Movil/tree/main/actividad5" },
         { numero: 6, subtareas: ["Investigación", "Planificación"], githubUrl: "https://github.com/tuUsuario/tuRepositorio/semana1" },
         { numero: 7, subtareas: ["Investigación", "Planificación"], githubUrl: "https://github.com/tuUsuario/tuRepositorio/semana1" },
         { numero: 8, subtareas: ["Investigación", "Planificación"], githubUrl: "https://github.com/tuUsuario/tuRepositorio/semana1" },
